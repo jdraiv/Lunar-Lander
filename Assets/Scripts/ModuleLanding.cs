86196b18453e31ed4c8e5435631200196e124b29
Josue Rivera <josuedr11@gmail.com>
2017-03-29 13:55:49 -0430

    Initial commit

---
+++ Assets/Scripts/ModuleLanding.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleLanding : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		if (coll.gameObject.tag == "Floor")
+		{
+            rb.velocity = Vector3.zero;
+			rb.Sleep();
+			Debug.Log("Failed");
+		}
+
+		if (coll.gameObject.tag == "Pad")
+		{
+			rb.velocity = Vector3.zero;
+			rb.Sleep();
+			Debug.Log("Success");
+		}
+	}
+}
---
+++ Assets/Scripts/ModuleMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleMovement : MonoBehaviour {
+	public float thrusterPower;
+	private float getLocation;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement();
+		Propulsion();
+	}
+
+	void Movement() {
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward * -5);
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			transform.Rotate(Vector3.forward * 5);
+		}
+	}
+
+	void Propulsion() {
+
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		getLocation = transform.rotation.eulerAngles.z;
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			
+
+			if ( getLocation == 0f){
+				rb.velocity = Vector3.zero;
+				rb.Sleep();
+				rb.AddForce(Vector2.up * thrusterPower);
+			}
+
+			else if (getLocation >= 5f & getLocation <= 50f)
+			{
+				rb.velocity = Vector3.zero;
+				rb.Sleep();
+				rb.AddForce(Vector2.left * thrusterPower);
+			}
+
+			else if (getLocation > 50f)
+			{
+				rb.velocity = Vector3.zero;
+				rb.Sleep();
+				rb.AddForce(Vector2.right * thrusterPower);
+			}
+		}
+	}
+}
